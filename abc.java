8917b8d0c404bcdc61d2257b835a7e42c2d11591
venugopalj1 <venugopaljoshi007@gmail.com>
2023-04-23 12:50:33 +0530

    files added

---
+++ abc.java
@@ -0,0 +1,9 @@
+public @interface abc {// Your First Program
+
+    class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!"); 
+        }
+    }
+    
+}
